3c637a207cc610947b601038ad5a945e5cd30ece
yickson <yicksonr@gmail.com>
2017-12-06 15:57:16 -0300

    vista de input dinamico completo

--- default/public/javascript/agregar.js
+++ default/public/javascript/agregar.js
@@ -7,12 +7,10 @@ $(document).ready(function(){
      //New input field html
     $(addButton).click(function(){ //Once add button is clicked
         if(x < maxField){ //Check maximum number of input fields
-          var fieldHTML = '<div class="'+ i +'"><div class="row"><div class="col-xs-12 col-sm-6"><div class="form-group"><label class="col-form-label" for="inputDefault">RUT</label><input type="text" class="form-control" placeholder="11.111.111-1" id="inputDefault"></div></div></div><a href="javascript:void(0);" class="remove_button btn btn-info btn-circle" title="'+i+'"><i class="fa fa-minus" aria-hidden="true"></i></a></div>';
+          var fieldHTML = '<div class="'+i+'"><div class="row"><div class="col-xs-12 col-sm-6"><div class="form-group"><label class="col-form-label" for="inputDefault">RUT</label><input name="rut'+i+'" class="form-control" type="text" placeholder="11.111.111-1" /></div></div><div class="col-xs-12 col-sm-6"><div class="form-group"><label class="col-form-label" for="inputDefault">NOMBRE COMPLETO</label><input name="nombre'+i+'" class="form-control" type="text" placeholder="Papelucho Paletin" /></div></div></div><div class="row"><div class="col-xs-12 col-sm-6"><div class="form-group"><label class="col-form-label">CORREO</label><input name="correo'+i+'" class="form-control" type="text" placeholder="" /></div></div><div class="col-xs-12 col-sm-6"><div class="form-group"><label class="col-form-label">CURSO</label><select name="curso'+i+'" class="form-control"><option value="1">Primero Basico</option><option value="2">Segundo Basico</option><option value="3">Tercero Basico</option><option value="4">Cuarto Basico</option><option value="5">Quinto Basico</option><option value="6">Sexto Basico</option><option value="7">Septimo Basico</option><option value="8">Octavo Basico</option><option value="9">Primero Medio</option><option value="10">Segundo Medio</option><option value="11">Tercero Medio</option><option value="12">Cuarto Medio</option></select></div></div></div><a href="javascript:void(0)" class="remove_button btn btn-danger btn-circle" title="'+i+'"><i class="fa fa-minus" aria-hidden="true"></a></div>';
             $(wrapper).append(fieldHTML); // Add field html
-            console.log($('.remove_button').attr('title'));
             x++; //Increment field counter
             i++;
-            console.log('Valor de I: '+i+ ' Valor de x: '+x);
         }
     });
     $(wrapper).on('click', '.remove_button', function(e){ //Once remove button is clicked
@@ -21,6 +19,5 @@ $(document).ready(function(){
         $('.'+capa).remove(); //Remove field html
         x--; //Decrement field counter
         i--;
-        console.log('Valor final i: '+i +' Valor de x'+x);
     });
 });

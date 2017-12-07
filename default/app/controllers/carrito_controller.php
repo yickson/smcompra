dc6655ee5a1d2c9e08426880ac389701f8cf5a07
crojas7 <informatik.rojas@gmail.com>
2017-12-07 17:26:38 -0300

    carrito mejora v2

--- default/app/controllers/carrito_controller.php
+++ default/app/controllers/carrito_controller.php
@@ -1,30 +1,45 @@
 <?php
 
 /**
- * Controlador para los usuarios
+ * Controlador para gestionar el carrito de compra
  */
-class UsuarioController extends AppController
+class CarritoController extends AppController
 {
   function before_filter()
   {
-    View::template('main');
+    View::template('carrito');
   }
   public function index()
   {
     //Esta vista no debería cargar nada
   }
-
-  public function datos()
-  {
-    //Identidad del apoderado
+  
+  /**
+   * Retorna información del alumno
+   * @param  $id post(int)
+   * @return json
+   **/
+  public function getAlumno(){
+      $id = $_POST["id"];
+      $alumno = (new Alumnos)->find($id);
+      $nombre_establecimiento = (new Establecimientos)->getNombreById($alumno->establecimiento_id);
+      $alumno->establecimiento_nombre = $nombre_establecimiento;
+      $this->data = $alumno;
+      View::select(null,"json");
   }
-
-  public function alumno()
-  {
-    //Identidad del alumno
+  
+  /**
+   * Retorna Pproductos de los hijos del profesor
+   * @param  $id post(int)
+   * @return json
+   **/
+  public function getProductos(){
+      $id_profesor = $_POST["id_profesor"];
+      $alumnos = (new Alumnos)->find_all_by_sql("");
+      
+      $this->data = $alumnos;
+      View::select(null,"json");
   }
 }
-
-
 
 ?>
--- default/app/models/establecimientos.php
+++ default/app/models/establecimientos.php
@@ -5,7 +5,15 @@
  */
 class Establecimientos extends ActiveRecord
 {
-  
+    
+ /**
+  * @param $id int
+  * @return string Envia el id del colegio para retornar su nombre
+  */
+  public function getNombreById($id){
+    $establecimiento = (New Establecimientos)->find($id);
+    return $establecimiento->nombre;
+  }
 }
 
 

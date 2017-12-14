8521edbb9fcbe040db86df245f169075a3ac57df
crojas7 <informatik.rojas@gmail.com>
2017-12-14 10:07:57 -0300

    correccion3

--- default/app/models/carrito.php
+++ default/app/models/carrito.php
@@ -31,6 +31,7 @@ class Carrito extends ActiveRecord
 	$productos_sql = new Productos;
 	$productos = array();
 	$result = null;
+	$total_format = null;
 	$total = null;
 	$productos_format = array();
 	$productos_arr = explode(",", $_POST["arr"]);
@@ -40,7 +41,8 @@ class Carrito extends ActiveRecord
 	    $productos_format[$i]["imagen"] = datatableAcciones::getImagen($result->imagen);
 	    $productos_format[$i]["descripcion"] = $result->descripcion;
 	    $productos_format[$i]["cantidad"] = 1;
-	    $total += $this->total($result->valor);
+	    $total = $this->total($result->valor);
+	    $total_format += $total;
 	    $productos_format[$i]["total"] = $this->formatNumeros($total);
 	    $productos_format[$i]["boton"] = datatableAcciones::getBtnCarrito($result->id);
 	    $i++;
@@ -50,8 +52,8 @@ class Carrito extends ActiveRecord
 	$subtotal = $this->formatNumeros($subtotal_decimal);
 	$iva = round($subtotal_decimal * 0.19);
 	$iva = $this->formatNumeros($iva);
-	$total = $this->valorDespacho($total);
-	$total = $this->formatNumeros($total);
+	$total = $this->valorDespacho($total_format);
+	$total = $this->formatNumeros($total_format);
 	$productos["data"] = datatableAcciones::getTotal($i, $productos_format, $subtotal, $iva, $total);
 	return  $productos;
     }

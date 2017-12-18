ba76f6ad62ef6c3cc6a523c8c176fe361e2b0d58
crojas7 <informatik.rojas@gmail.com>
2017-12-18 18:31:02 -0300

    mejoras

--- default/app/models/productos.php
+++ default/app/models/productos.php
@@ -19,14 +19,14 @@ class Productos extends ActiveRecord
       switch($tipo):
 	  case $this::APODERADO: 
 	        $alumnos = (new Productos)->find_all_by_sql("SELECT p.id as id_producto, p.nombre as asignatura, p.proyecto, p.nivel, p.imagen as img,
-							  (SELECT nombre FROM productos_tipo WHERE id = p.tipo) as tipo, p.valor, li.alumno_id as id_alumno
+							  (SELECT nombre FROM productos_tipo WHERE id = p.tipo) as tipo, p.valor, li.alumno_id as id_alumno, li.estado
 							   FROM productos as p
 							   INNER JOIN licences li ON (li.producto_id = p.id and li.usuario_id = $id_usuario)
 							   ORDER BY li.alumno_id ASC");
 	  break;
 	  case $this::PROFESOR:
 	        $alumnos = (new Productos)->find_all_by_sql("SELECT p.id as id_producto, p.nombre as asignatura, p.proyecto, p.nivel, p.imagen as img,
-							   (SELECT nombre FROM productos_tipo WHERE id = p.tipo) as tipo, p.valor, pa.alumno_id as id_alumno
+							   (SELECT nombre FROM productos_tipo WHERE id = p.tipo) as tipo, p.valor, pa.alumno_id as id_alumno, li.estado
 							    FROM productos as p
 							    INNER JOIN profesor_alumnos pa ON (pa.producto_id = p.id and pa.usuario_id = $id_usuario)
 							    ORDER BY pa.alumno_id ASC");

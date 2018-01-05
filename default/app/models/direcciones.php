ab1aac5923e2bcf4d2d97ad1c01ead7dbd5c5d47
crojas7 <informatik.rojas@gmail.com>
2018-01-05 15:26:57 -0300

    mejoras

--- default/app/models/direcciones.php
+++ default/app/models/direcciones.php
@@ -5,11 +5,22 @@
  */
 class Direcciones extends ActiveRecord
 {
+    
   /**
-   * @return object devuelve una instancia de direcciones de usuario logeado
+   * @return object devuelve una instancia de direcciones de usuario logeado Solo ID's
    */  
   public function getDireccion(){
       $direccion = (new Direcciones)->find_by_id_usuario(Session::get("iduser"));
+      return $direccion; 
+  }
+  
+  /**
+   * @return object devuelve una instancia de direcciones de usuario logeado con nombre de Comuna y Region
+   */  
+  public function getFullDireccion(){
+      $direccion = (new Direcciones)->find_by_id_usuario(Session::get("iduser"));
+      $direccion->nombre_region = (new Regiones)->find($direccion->id_region)->nombre;
+      $direccion->nombre_comuna = (new Comunas)->find($direccion->id_comuna)->nombre;
       return $direccion; 
   }
   

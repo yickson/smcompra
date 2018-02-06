115eb996b7bf35f973336f9ee05d0711d6ccfd54
Yickson Ramirez <yicksonr@gmail.com>
2018-02-06 18:07:17 -0300

    ingresos y total despacho

--- default/app/controllers/administrador/webpay_controller.php
+++ default/app/controllers/administrador/webpay_controller.php
@@ -30,6 +30,15 @@ class WebpayController extends AppController
 
   }
 
+  public function ingresos()
+  {
+    //Ventas totales, despacho
+    $this->ventatotal = (New WebpayTransaccion)->find_by_sql("SELECT SUM(monto) as venta FROM webpay_transaccion WHERE codigoRespuesta = 0 ");
+    $despacho = (New WebpayTransaccion)->find_by_sql("SELECT COUNT(*) as transaccion FROM webpay_transaccion wt INNER JOIN usuarios u ON (u.id = wt.usuario_id AND u.tipo = 2)");
+    $totaldespacho = ($despacho->transaccion * 3090);
+    $this->despacho = $totaldespacho;
+  }
+
   //Métodos para AJAX
 
   public function listar_operaciones()
